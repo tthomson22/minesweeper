1867f97df56a029cd34bc997908e8210b1b3ddb6
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-12 20:29:48 -0700

    random mines are set

--- minesweeper.js
+++ minesweeper.js
@@ -8,6 +8,9 @@ const TITLE_STATUSES = {
 
 export function createBoard(boardSize, numberOfMines){
     const board = []
+    const minePositions = getMinePositions(boardSize, numberOfMines)
+    
+
     for(let x = 0; x < boardSize; x++){
         const row = []
         for(let y = 0; y < boardSize; y++){
@@ -18,6 +21,8 @@ export function createBoard(boardSize, numberOfMines){
                 element,
                 x,
                 y,
+                //checks if position matches x and y coordinate
+                mine: minePositions.some(positionMatch.bind(null, { x, y })),
                 get status(){
                     return element.dataset.status
                 },
@@ -30,4 +35,28 @@ export function createBoard(boardSize, numberOfMines){
         board.push(row)
     }
     return board
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+    const positions = []
+
+    while(positions.length < numberOfMines){
+        const position = {
+            x: randomNumber(boardSize),
+            y: randomNumber(boardSize)
+        }
+
+        if(!positions.some(p => positionMatch(p, position))){
+            positions.push(position)
+        }
+    }
+    return positions
+}
+
+function positionMatch(a, b){
+    return a.x === b.x && a.y === b.y
+}
+
+function randomNumber(size){
+    return Math.floor(Math.random() * size)
 }
