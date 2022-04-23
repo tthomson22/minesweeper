dea521297a11fada57171c2ff9e4e32969c061bc
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-23 13:51:53 -0700

    mines left text now displays the value of NUMBER_OF_MINES

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 import { createBoard } from './minesweeper.js'
 
 const BOARD_SIZE = 5
-const NUMBER_OF_MINES = 2
+const NUMBER_OF_MINES = 3
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
 const boardElement = document.querySelector('.board')
-// adds styling to grid
-boardElement.style.setProperty('--size', BOARD_SIZE)
+const minesLeft = document.querySelector('[data-mine-count]')
+
 
 console.log(board)
 
@@ -15,3 +15,7 @@ board.forEach(row => {
         boardElement.append(tile.element)
     })
 })
+
+// adds styling to grid
+boardElement.style.setProperty('--size', BOARD_SIZE)
+minesLeft.textContent = NUMBER_OF_MINES
